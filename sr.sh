764a1b334af5dcc543fcde1f425f9373f02f6f4f
Your Name <you@example.com>
2026-03-30 15:09:56 +0800

    update source list file name

--- sr.sh
+++ sr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
-echo "deb [trusted=yes] file:$(realpath $(pwd)) ./" > /etc/apt/sources.list.d/local-src.list
+echo "deb [trusted=yes] file:$(realpath $(pwd)) ./" > /etc/apt/sources.list.d/a-src.list
