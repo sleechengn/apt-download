ee69d3f52e675730a69738557de79234538ca869
root <root@pve.lan>
2026-04-16 11:01:03 +0800

    更新

--- sr.sh
+++ sr.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
+OSS_T=$(cat /etc/os-release |grep "^ID="|tr -d \"|awk -F = '{print $2}')
+VER_T=$(cat /etc/os-release |grep "^VERSION_ID="|tr -d \"|awk -F = '{print $2}')
+ROOT_DIR=$(realpath $(pwd))/debs/$OSS_T/$VER_T
+cd $ROOT_DIR
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
-echo "deb [trusted=yes] file:$(realpath $(pwd)) ./" > /etc/apt/sources.list.d/a-src.list
+echo "deb [trusted=yes] file:$(realpath $(pwd)) ./" > /etc/apt/sources.list.d/a-local-src.list
