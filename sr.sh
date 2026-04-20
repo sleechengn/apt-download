f6428dd0705dae6d3d4858b042e241853a900f87
leecheng <sleechengn@live.com>
2026-04-20 12:18:32 +0000

    保存程序

--- sr.sh
+++ sr.sh
@@ -3,5 +3,6 @@ OSS_T=$(cat /etc/os-release |grep "^ID="|tr -d \"|awk -F = '{print $2}')
 VER_T=$(cat /etc/os-release |grep "^VERSION_ID="|tr -d \"|awk -F = '{print $2}')
 ROOT_DIR=$(realpath $(pwd))/debs/$OSS_T/$VER_T
 cd $ROOT_DIR
+rm -rf tmp
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 echo "deb [trusted=yes] file:$(realpath $(pwd)) ./" > /etc/apt/sources.list.d/a-local-src.list
