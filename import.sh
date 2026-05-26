63e0df2e207d9662658b3f2b1d62c118a9fbffc5
alicia <sleechengn@live.cn>
2026-05-26 15:19:52 +0800

    导入

--- import.sh
+++ import.sh
@@ -8,7 +8,8 @@ if [ "$TARGET" ]; then
 for f in $TARGET/*.deb; do
 	pack=$(dpkg -I $f|grep "Package:"|awk '{print $2}')
 	pack_dir=$(echo $pack|sed 's/:/%3A/g')
-	fn=$(echo $f|awk -F "/" '{print $2}')
+	fn=$(echo $f|awk -F "/" '{print $NF}')
+	echo "处理文件：$fn"
 	if [ -e "$(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir" ]; then
 		echo "已经存在包:$pack_dir"
 		if [ -e "$(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir/$fn" ]; then
@@ -16,7 +17,7 @@ for f in $TARGET/*.deb; do
 			else
 			cp -ra $f $(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir
 		fi
-		else
+	else
 		mkdir $(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir
 		cp -ra $f $(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir
 	fi
