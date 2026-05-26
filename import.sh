6a89298108370c3fbf25eeaa849ab1c7305983c5
alicia <sleechengn@live.cn>
2026-05-26 15:01:37 +0800

    导入

--- import.sh
+++ import.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/bash
+set -e
 TARGET=$1
+OSS_T=$(cat /etc/os-release |grep "^ID="|tr -d \"|awk -F = '{print $2}')
+VER_T=$(cat /etc/os-release |grep "^VERSION_ID="|tr -d \"|awk -F = '{print $2}')
+
 if [ "$TARGET" ]; then
 for f in $TARGET/*.deb; do
 	pack=$(dpkg -I $f|grep "Package:"|awk '{print $2}')
 	pack_dir=$(echo $pack|sed 's/:/%3A/g')
 	fn=$(echo $f|awk -F "/" '{print $2}')
-	if [ -e "$(dirname $0)/debs/debian/13/$pack_dir" ]; then
+	if [ -e "$(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir" ]; then
 		echo "已经存在包:$pack_dir"
-		if [ -e "$(dirname $0)/debs/debian/13/$pack_dir/$fn" ]; then
+		if [ -e "$(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir/$fn" ]; then
 			echo "已经存在:$fn"
 			else
-			cp -ra $f $(dirname $0)/debs/debian/13/$pack_dir
+			cp -ra $f $(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir
 		fi
 		else
-		mkdir $(dirname $0)/debs/debian/13/$pack_dir
-		cp -ra $f $(dirname $0)/debs/debian/13/$pack_dir
+		mkdir $(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir
+		cp -ra $f $(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir
 	fi
 done
 fi
