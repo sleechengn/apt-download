3d9e1267c95e4dcbc643b7f358571d3980fa937d
leecheng <sleechengn@live.com>
2026-05-31 04:53:19 +0000

    修正导入时

--- import.sh
+++ import.sh
@@ -6,9 +6,14 @@ VER_T=$(cat /etc/os-release |grep "^VERSION_ID="|tr -d \"|awk -F = '{print $2}')
 
 if [ "$TARGET" ]; then
 for f in $TARGET/*.deb; do
-	pack=$(dpkg -I $f|grep "Package:"|awk '{print $2}')
+	pack=$(dpkg -I $f|grep "Package:"|grep -v "Auto-Built-Package"|awk '{print $2}')
 	pack_dir=$(echo $pack|sed 's/:/%3A/g')
 	fn=$(echo $f|awk -F "/" '{print $NF}')
+
+	echo "current file: $fn"
+	echo "current package: $pack"
+	echo "pack dir: $pack_dir"
+
 	if [ -e "$(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir" ]; then
 		if [ -e "$(dirname $0)/debs/$OSS_T/$VER_T/$pack_dir/$fn" ]; then
 			echo "exist: $fn"
